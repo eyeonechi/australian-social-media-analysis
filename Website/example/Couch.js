ad3be3dbb410834b7bc6186a51b2e6bf21354112
sakuya9t <sakuya9t@vip.qq.com>
2018-05-05 20:16:45 +1000

    Add files via upload

---
+++ Website/example/Couch.js
@@ -0,0 +1,36 @@
+function Couch(){
+
+    /*Query data with specific selection*/
+    this.query = function(db_name, selector){
+        console.log(selector);
+        var result;
+        $.ajax({
+            type: "POST",
+            async: false,
+            url: "http://115.146.85.206:5000/database/" + db_name + "/query",
+            ContentType: "application/json",
+            data: JSON.stringify(selector),
+            dataType: "json",
+            success: function(data) {
+                result = data;
+            }
+        });
+        return result;
+    };
+
+    this.query_all = function(db_name){
+        var result;
+        $.ajax({
+            type: "GET",
+            async: false,
+            url: "http://115.146.85.206:5000/database/" + db_name,
+            ContentType: "application/json",
+            data: {},
+            dataType: "json",
+            success: function(data) {
+                result = data;
+            }
+        });
+        return result;
+    }
+}
