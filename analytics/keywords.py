8e25e096ba02eb2444e78b56cafa741336d8c117
duerw <minghaow1@student.unimelb.edu.au>
2018-05-07 00:49:53 +1000

    added regression model and included aurin data

---
+++ analytics/keywords.py
@@ -0,0 +1,272 @@
+
+class Keywords:
+    
+    fastfood = [
+        "burrito",
+        "chips",
+        "fries",
+        "hamburger",
+        "hot dog",
+        "nacho",
+        "nugget",
+        "onion rings",
+        "pizza",
+        "sandwich",
+        "sausage",
+        "taco"
+    ]
+
+    fruits = [
+        "apple",
+        "apricot",
+        "avocado",
+        "banana",
+        "berry",
+        "blackberry",
+        "blood orange",
+        "blueberry",
+        "boysenberry",
+        "breadfruit",
+        "cantaloupe",
+        "cherry",
+        "citron",
+        "citrus",
+        "coconut",
+        "crabapple",
+        "cranberry",
+        "current",
+        "date",
+        "dragonfruit",
+        "durian",
+        "elderberry",
+        "fig",
+        "grape",
+        "grapefruit",
+        "guava",
+        "honeydew",
+        "jackfruit",
+        "kiwi",
+        "kumquat",
+        "lemon",
+        "lime",
+        "lingonberry",
+        "loquat",
+        "lychee",
+        "mandarin",
+        "mango",
+        "mangosteen",
+        "marionberry",
+        "melon",
+        "mulberry",
+        "nectarine",
+        "orange",
+        "papaya",
+        "passion fruit",
+        "peach",
+        "pear",
+        "persimmon",
+        "pineapple",
+        "plantain",
+        "plum",
+        "pluot",
+        "pomegranite",
+        "pomelo",
+        "prune",
+        "quince",
+        "raisin",
+        "rambutan",
+        "raspberry",
+        "starfruit",
+        "strawberry",
+        "tangelo",
+        "tangerine",
+        "ugli fruit",
+        "watermelon"
+    ]
+
+    grains = [
+        "amaranth"
+        "barley"
+        "bread"
+        "brown rice"
+        "buckwheat"
+        "bulgur"
+        "cornbread"
+        "couscous"
+        "crackers"
+        "emmer"
+        "farro"
+        "flaxseed"
+        "grano"
+        "grits"
+        "kamut grain"
+        "millet"
+        "oats"
+        "oatmeal"
+        "popcorn"
+        "macaroni"
+        "muesli"
+        "noodles"
+        "pitas"
+        "pretzels"
+        "quinoa"
+        "rolled oats"
+        "rice"
+        "rye"
+        "sorghum"
+        "spaghetti"
+        "spelt"
+        "teff"
+        "tortillas"
+        "triticale"
+        "whole grain"
+        "wheat berries"
+        "whole rye"
+        "whole wheat"
+        "wild rice"
+    ]
+
+    meat = [
+        "bacon",
+        "beef",
+        "chicken",
+        "duck",
+        "ham",
+        "mutton",
+        "pork",
+        "sausage",
+        "steak",
+        "turkey",
+        "veal",
+        "venison",
+    ]
+
+    seafood = [
+        "abalone",
+        "anchovy",
+        "bass",
+        "caviar",
+        "clam",
+        "cockle",
+        "cod",
+        "crab",
+        "crayfish",
+        "eel",
+        "gemfish",
+        "herring",
+        "lobster",
+        "mackerel",
+        "mussel",
+        "octopus",
+        "oyster",
+        "prawn",
+        "salmon",
+        "sardine",
+        "scallop",
+        "sea cucumber",
+        "shark",
+        "shellfish",
+        "shrimp",
+        "squid",
+        "trout",
+        "tuna"
+    ]
+
+    vegetables = [
+        "acorn squash",
+        "alfalfa",
+        "artichoke",
+        "arugula",
+        "asparagus",
+        "avocado",
+        "bamboo shoots",
+        "basil",
+        "beans",
+        "beets",
+        "bell pepper",
+        "black-eyed peas",
+        "broccoli",
+        "brussels sprouts",
+        "capers",
+        "carrot",
+        "cauliflower",
+        "celeriac",
+        "celery",
+        "chard",
+        "chick peas",
+        "chinese cabbage",
+        "chives",
+        "collard greens",
+        "cress",
+        "cucumber",
+        "daikon",
+        "dandelion greens",
+        "eggplant",
+        "endive",
+        "fava bean",
+        "fennel",
+        "garlic",
+        "ginger",
+        "gourd",
+        "greenbean",
+        "greens",
+        "hot chile peppers",
+        "iceberg lettuce",
+        "jicama",
+        "kale",
+        "kohlrabi",
+        "leek",
+        "lentils",
+        "lettuce",
+        "lima bean",
+        "maize",
+        "mung bean",
+        "mushroom",
+        "mustard greens",
+        "okra",
+        "olive",
+        "onion",
+        "parsley",
+        "parsnip",
+        "pattypan squash",
+        "pea",
+        "peapod",
+        "peanut",
+        "peppers",
+        "pickle",
+        "potato",
+        "pumpkin",
+        "radicchio",
+        "radish",
+        "rhubarb",
+        "rocket",
+        "romaine",
+        "rutabaga",
+        "salad",
+        "salsa",
+        "scallion",
+        "seaweed",
+        "shallot",
+        "sorrel",
+        "soybean",
+        "spinach",
+        "sprouts",
+        "salsify",
+        "spuds",
+        "squash",
+        "string bean",
+        "succotash",
+        "sweet potato",
+        "swiss chard",
+        "taro",
+        "tomatillo",
+        "tomato",
+        "tuber",
+        "turnip",
+        "vegetable",
+        "wasabi",
+        "water chestnut",
+        "watercress",
+        "yam",
+        "zucchini",
+    ]
