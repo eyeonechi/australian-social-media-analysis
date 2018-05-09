1b536d1e64b1e8828f109c463666b3e6f6f24b3c
Ivan Ken Weng Chee <xenzaki@msn.com>
2018-05-09 15:51:29 +1000

    Updated stuff

--- website2/match_polygon.py
+++ website2/match_polygon.py
@@ -7,16 +7,7 @@ with open('geojson/aus_lga2.json') as f:
     js = json.load(f)
 with open('geojson/homeless.json') as f:
     hl = json.load(f)
-'''
-# construct point based on lon/lat returned by geocoder
-point = Point(144.962228, -37.799451)
 
-# check each polygon to see if it contains the point
-for feature in js['features']:
-    polygon = shape(feature['geometry'])
-    if polygon.contains(point):
-        print 'Found containing polygon:', feature
-'''
 i = 0
 with open("geojson/aus_lga2_merge.json", "w") as output:
     for data in hl['data']:
@@ -32,11 +23,3 @@ with open("geojson/aus_lga2_merge.json", "w") as output:
                 outfeature['cnt2016'] = data['cnt2016']
                 output.write(json.dumps(outfeature) + ',\n')
             i += 1
-
-'''
-with open("geojson/aus_lga2.json", "r") as input:
-
-        features = json.load(input)['features']
-        for i in range(len(features)):
-            print(features[i]['properties']['Name'])
-'''
