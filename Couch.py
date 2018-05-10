8db8595fad1bbc3664382b38d3518aaa8e18f776
teamfortytwo <39042026+teamfortytwo@users.noreply.github.com>
2018-05-10 20:36:28 +1000

    Add files via upload

---
+++ Couch.py
@@ -0,0 +1,284 @@
+"""
+@Team info:
+    CCC Team 42 (Melbourne)
+    Hannah Ha   963370
+    Lan Zhou    824371
+    Zijian Wang 950618
+    Ivan Chee   736901
+    Duer Wang   824325
+@Module: Couch
+@Function: Provide an api with couchDB using cloudant.
+@Methods:
+    select_db: select a database, create it if not exist.
+    close: close current database.
+    count: get the count of items in current database.
+    query_all: get all stuffs in current database.
+    query: query current database with given selector, return result.
+    insert: add something to current database.
+    update: change all items in current database with given condition.
+    delete: delete all items from current database satisfying given selector.
+    aggr_sentiment_analysis: get result from a map&reduced custom view.
+                             View details are in MapReduce.js
+"""
+from cloudant.client import CouchDB
+from cloudant.database import CloudantDatabase
+from cloudant.document import Document
+from cloudant.database import CouchDatabase
+from cloudant.design_document import DesignDocument
+
+
+class Couch:
+    db = None
+    c_db = None
+    couch_db = None
+
+    # usage: database = Couch(db_name)
+    # fields: db_name -> str
+    def __init__(self, db_name):
+        self.client = CouchDB('aassddff', 'ffddssaa', url='http://115.146.85.206:5984/', connect=True, auto_renew=True)
+        self.select_db(db_name)
+
+    # Get one database selected; if the database doesn't exist, create it.
+    # usage: database.select_db(db_name);
+    # fields: db_name -> str
+    def select_db(self, db_name):
+        self.couch_db = CouchDatabase(self.client, db_name)
+        if not self.couch_db.exists():
+            self.couch_db.create()
+        self.db = self.client[db_name]
+        self.c_db = CloudantDatabase(self.client, db_name)
+
+    # usage: database.close()
+    # Database should be closed when finish using
+    def close(self):
+        self.client.disconnect()
+
+    # Get count of documents in current database;
+    # usage database.count();
+    def count(self):
+        return self.couch_db.doc_count()
+
+    # Get everything from the database;
+    # usage: database.query_all();
+    # note: after query_all, iterate the returned item to get every document
+    def query_all(self):
+        qlist = []
+        for doc in self.db:
+            qlist.append(doc)
+        return qlist
+
+    # Select something from the database;
+    # usage: database.query(selector);
+    # fields: selector -> Dictionary
+    # note: after query, iterate the returned item to get every document
+    def query(self, selector):
+        qlist = []
+        result = self.c_db.get_query_result(selector)
+        for doc in result:
+            qlist.append(doc)
+        return qlist
+
+    # insert operation of the database;
+    # usage: database.insert(doc);
+    # fields: doc -> Dictionary
+    def insert(self, doc):
+        document = self.db.create_document(doc)
+        if document.exists():
+            return document['id']
+
+    # update operation of the database;
+    # usage: database.update(field, old_value, new_value)
+    # fields: field -> str; value -> str; new_value -> str
+    def update(self, field, value, new_value):
+        selector = {field: value}
+        q_res = self.c_db.get_query_result(selector)
+        for document in q_res:
+            id = document['_id']
+            doc = Document(self.db, id)
+            doc.update_field(
+                action=doc.field_set,
+                field=field,
+                value=new_value
+            )
+
+    # delete operation of the database;
+    # usage: database.delete(selector)
+    # fields: selector -> Dictionary
+    def delete(self, selector):
+        q_res = self.c_db.get_query_result(selector)
+        for document in q_res:
+            id = document['_id']
+            rev = document['_rev']
+            doc = Document(self.db, id)
+            doc['_rev'] = rev
+            doc.delete()
+
+    # get aggregated data using map&reduce to get average sentiment score of different time periods
+    # the view has been already created on the couchDB server.
+    def get_aggr_view(self, view_name):
+        dd = self.db.get_design_document("_design001")
+        view = dd.get_view(view_name)
+        if view is None:
+            self.set_aggr_view(view_name)
+            dd = self.db.get_design_document("_design001")
+            view = dd.get_view("view1")
+        return view.result[0][0]["value"]
+
+    # set the view of analyzing sentiment score with time periods using built-in map&reduce function.
+    def set_aggr_view(self, view_name):
+        if view_name == "view1":
+            map_func = """
+                function (doc) {
+                var t = doc.created_at.time.split(":");
+                if(parseInt(t) < 6 ) t = "Midnight";
+                else if(parseInt(t) < 12) t = "Morning";
+                else if(parseInt(t) < 18) t = "Afternoon";
+                else t = "Evening";
+                emit([t], doc.polarity);
+                }
+            """
+            reduce_func = """
+                function (keys, values, rereduce){
+                var key_set = ["Midnight", "Afternoon", "Morning", "Evening"]
+                var dict = {}
+                for(var i = 0; i < key_set.length; i++) {
+                dict[key_set[i]] = {"sum":0, "cnt":0, "avg":0};
+                }
+                if (rereduce) {
+                for(var j = 0; j < values.length; j++){
+                for(var i = 0; i < key_set.length; i++) {
+                dict[key_set[i]]["sum"] += values[j][key_set[i]]["sum"];
+                dict[key_set[i]]["cnt"] += values[j][key_set[i]]["cnt"];
+                }
+                }
+                for(var i = 0; i < key_set.length; i++)
+                dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                return dict;
+                }
+                else {
+                for(var i = 0; i < key_set.length; i++) {
+                for(var j = 0; j < values.length; j++){
+                  if(key_set[i] == keys[j][0][0]){
+                    dict[key_set[i]]["sum"] += values[j];
+                    dict[key_set[i]]["cnt"] += 1;
+                    dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                  }
+                }
+                }
+                return dict;
+                }
+                }
+            """
+
+        elif view_name == "view2":
+            map_func = """
+                                function (doc) {
+                                var t = doc.created_at.weekday;
+                                emit([t], doc.polarity);
+                                }
+                                }
+                            """
+            reduce_func = """
+                                function (keys, values, rereduce){
+                                var key_set = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+                                var dict = {}
+                                for(var i = 0; i < key_set.length; i++) {
+                                dict[key_set[i]] = {"sum":0, "cnt":0, "avg":0};
+                                }
+                                if (rereduce) {
+                                for(var j = 0; j < values.length; j++){
+                                for(var i = 0; i < key_set.length; i++) {
+                                dict[key_set[i]]["sum"] += values[j][key_set[i]]["sum"];
+                                dict[key_set[i]]["cnt"] += values[j][key_set[i]]["cnt"];
+                                }
+                                }
+                                for(var i = 0; i < key_set.length; i++)
+                                dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                                return dict;
+                                }
+                                else {
+                                for(var i = 0; i < key_set.length; i++) {
+                                for(var j = 0; j < values.length; j++){
+                                if(key_set[i] == keys[j][0][0]){
+                                dict[key_set[i]]["sum"] += values[j];
+                                dict[key_set[i]]["cnt"] += 1;
+                                dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                                }
+                                }
+                                }
+                                return dict;
+                                }
+                                }
+                            """
+        else:
+            map_func = """
+                            function (doc) {
+                            var t = doc.created_at.month + "-" + doc.created_at.day;
+                            emit([t], doc.polarity);
+                            }
+                            """
+            reduce_func = """
+                            function (keys, values, rereduce){
+                            var key_set = []
+                            var dict = {}
+                            for(var i = 0; i < key_set.length; i++) {
+                            dict[key_set[i]] = {"sum":0, "cnt":0, "avg":0};
+                            }
+                            if (rereduce) {
+                            for(var i = 0; i < values.length; i++){
+                            for(prop in values[i]){
+                            if(!exists(prop, key_set)) key_set.push(prop);
+                            }
+                            }
+                            for(var i = 0; i < key_set.length; i++){
+                            dict[key_set[i]] = {};
+                            dict[key_set[i]]["sum"] = 0;
+                            dict[key_set[i]]["cnt"] = 0;
+                            for(var j = 0; j < values.length; j++) {
+                            if(key_set[i] in values[j]){
+                            dict[key_set[i]]["sum"] += values[j][key_set[i]]["sum"];
+                            dict[key_set[i]]["cnt"] += values[j][key_set[i]]["cnt"];
+                            }
+                            }
+                            }
+                            for(var i = 0; i < key_set.length; i++)
+                            if(key_set[i] in dict) dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                            return dict;
+                            }
+                            else {
+                            for(var i = 0; i < keys.length; i++){
+                            var k = keys[i][0][0];
+                            if(!exists(k, key_set)) key_set.push(k);
+                            }
+                            for(var i = 0; i < key_set.length; i++) {
+                            dict[key_set[i]] = {};
+                            dict[key_set[i]]["sum"] = 0;
+                            dict[key_set[i]]["cnt"] = 0;
+                            for(var j = 0; j < values.length; j++){
+                            if(key_set[i] === keys[j][0][0]){
+                            dict[key_set[i]]["sum"] += values[j];
+                            dict[key_set[i]]["cnt"] += 1;
+                            dict[key_set[i]]["avg"] = (dict[key_set[i]]["sum"] / dict[key_set[i]]["cnt"]);
+                            }
+                            }
+                            }
+                            return dict;
+                            }
+                            }
+                            
+                            function exists(e, arr){
+                            for(var i = 0; i < arr.length; i++){
+                            if(arr[i] === e) return true;
+                            }
+                            return false;
+                            }
+                        """
+        dd = DesignDocument(self.db, "_design001")
+        dd.add_view(view_name, map_func, reduce_func)
+        dd.save()
+
+
+if __name__ == "__main__":
+    conn = Couch("asdfasdf")
+    res = conn.get_aggr_view()
+    print(res)
---
+++ WebServer.py
@@ -0,0 +1,120 @@
+"""
+@Team info:
+    CCC Team 42 (Melbourne)
+    Hannah Ha   963370
+    Lan Zhou    824371
+    Zijian Wang 950618
+    Ivan Chee   736901
+    Duer Wang   824325
+@Module: WebServer
+@Function: Provide a webservice running on port 5000, webpages should only query database through this service.
+@Methods:
+    query_all: get all data with specified database name.
+    query: query current database with given selector, return result.
+    aggregate: get information of aggregated data of specific database.
+@Usage: should be running standalone.
+    python3 WebServer.py
+"""
+from flask import Flask, request
+import Couch
+import json
+import flask
+
+# start the web service.
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """
+    For test if the service works.
+    :return: NoneType
+    """
+    return "Hello, World!"
+
+
+@app.route('/database/<string:db_name>', methods=["GET"])
+def query_all(db_name):
+    """
+    get all data with specified database name.
+    :param db_name: string; the name of target database
+    :return: res: respond object including the query result.
+    """
+    db = Couch.Couch(db_name)
+    q_res = {"data": db.query_all()}
+    res = flask.make_response(flask.jsonify(q_res))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'GET'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return res
+
+
+@app.route('/database/<string:db_name>/query', methods=["POST"])
+def query(db_name):
+    """
+    query current database with given selector, return result.
+    :param db_name: string; the name of target database
+    :return: res: respond object including the query result.
+    """
+    data = request.get_data()
+    data = data.decode("utf-8")
+    selector = json.loads(data)
+    db = Couch.Couch(db_name)
+    q_res = {"data": db.query(selector)}
+    res = flask.make_response(flask.jsonify(q_res))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'POST'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return res
+
+
+@app.route('/database/<string:db_name>/period', methods=["GET"])
+def period_view(db_name):
+    """
+    get information of aggregated data of specific database.
+    :param db_name: string; the name of target database
+    :return: res: respond object including the query result.
+    """
+    db = Couch.Couch(db_name)
+    q_res = {"data": db.get_aggr_view("view1")}
+    res = flask.make_response(flask.jsonify(q_res))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'GET'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return res
+
+
+@app.route('/database/<string:db_name>/day', methods=["GET"])
+def day_view(db_name):
+    """
+    get information of aggregated data of specific database.
+    :param db_name: string; the name of target database
+    :return: res: respond object including the query result.
+    """
+    db = Couch.Couch(db_name)
+    q_res = {"data": db.get_aggr_view("view2")}
+    res = flask.make_response(flask.jsonify(q_res))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'GET'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return res
+
+
+@app.route('/database/<string:db_name>/date', methods=["GET"])
+def day_view(db_name):
+    """
+    get information of aggregated data of specific database.
+    :param db_name: string; the name of target database
+    :return: res: respond object including the query result.
+    """
+    db = Couch.Couch(db_name)
+    q_res = {"data": db.get_aggr_view("view3")}
+    res = flask.make_response(flask.jsonify(q_res))
+    res.headers['Access-Control-Allow-Origin'] = '*'
+    res.headers['Access-Control-Allow-Methods'] = 'GET'
+    res.headers['Access-Control-Allow-Headers'] = 'x-requested-with,content-type'
+    return res
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
