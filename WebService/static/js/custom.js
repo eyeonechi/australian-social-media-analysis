f63903a47dcdffe017e710d0ea633603765a26be
eyeonechi <xenzaki@msn.com>
2020-03-24 03:53:17 +0800

    Fix jquery updated syntax

--- WebService/static/js/custom.js
+++ WebService/static/js/custom.js
@@ -1,47 +1,41 @@
-
  /* jQuery Pre loader
   -----------------------------------------------*/
-$(window).load(function(){
-    $('.preloader').fadeOut(1000); // set duration in brackets
+$(window).on('load', function(){
+  $('.preloader').fadeOut(1000); // set duration in brackets
 });
 
-
 $(document).ready(function() {
-
   /* Home Slideshow Vegas
   -----------------------------------------------*/
   $(function() {
     $('body').vegas({
-        slides: [
-            { src: 'static/images/slide-1.jpg' },
-            { src: 'static/images/slide-2.jpg' }
-        ],
-        preloadImage: false,
-        timer: false,
-        transition: [ 'zoomOut' ]
+      slides: [
+        { src: 'static/images/slide-1.jpg' },
+        { src: 'static/images/slide-2.jpg' }
+      ],
+      preloadImage: false,
+      timer: false,
+      transition: [ 'zoomOut' ]
     });
   });
 
-
    /* Back top
   -----------------------------------------------*/
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 200) {
-        $('.go-top').fadeIn(200);
-        } else {
-          $('.go-top').fadeOut(200);
-        }
-        });   
-        // Animate the scroll to top
-      $('.go-top').click(function(event) {
-        event.preventDefault();
-      $('html, body').animate({scrollTop: 0}, 300);
-      })
-      
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $('.go-top').fadeIn(200);
+    } else {
+      $('.go-top').fadeOut(200);
+    }
+  });
+  
+  // Animate the scroll to top
+  $('.go-top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 300);
+  });
 
   /* wow
   -------------------------------*/
   new WOW({ mobile: false }).init();
-
-  });
-
+});
